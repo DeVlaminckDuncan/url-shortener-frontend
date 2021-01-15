b789ab9b8582d9fb2ad9eddbd29c73f1cf8bab1c
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-15 02:21:40 +0100

    Removed h1

--- src/presentations/url-shortener/views/Home.vue
+++ src/presentations/url-shortener/views/Home.vue
@@ -1,6 +1,4 @@
 <template>
-	<h1>Home</h1>
-
 	<main class="mt-6">
 		<p class="mb-2 text-lg">Welcome to URL Shortener!</p>
 		<p class="text-lg">Get started by <router-link to="/signup" class="text-blue-main">creating an account.</router-link></p>
