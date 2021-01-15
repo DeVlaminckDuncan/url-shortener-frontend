07c85760d93d2c177a1fedac227ac065dcac45b0
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-15 02:18:04 +0100

    Update index.ts
    Get & save user ID

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,26 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+
+export enum MutationTypes {
+	SET_USERID = 'setUserId',
+}
+
+const state = {
+	userId: localStorage.userId ? JSON.parse(localStorage.userId) : '',
+};
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: state,
+
+	getters: {
+		getUserId: (state) => () => {
+			return state.userId ? state.userId : null;
+		},
+	},
+
+	mutations: {
+		[MutationTypes.SET_USERID](state, userId) {
+			const data = userId;
+			localStorage.setItem('userId', JSON.stringify(data));
+		},
+	},
+});
