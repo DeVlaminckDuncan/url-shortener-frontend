686fc8f89fe8963c5d6285171f7f72112f905333
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-15 02:25:40 +0100

    Create CheckToken.vue
    This view checks if there's a token in the browser and based on that it goes to Home or Dashboard

---
+++ src/presentations/url-shortener/views/CheckToken.vue
@@ -0,0 +1,26 @@
+<template>
+	<router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import route from '@/router';
+
+import cookie from '@/utils/cookie';
+
+export default defineComponent({
+	setup() {
+		const checkTokenCookie = () => {
+			if (cookie.get('token') == '') {
+				route.replace('/home');
+			} else {
+				const path = ref(route.currentRoute.value.path).value as string;
+
+				route.replace('/dashboard');
+			}
+		};
+
+		checkTokenCookie();
+	},
+});
+</script>
