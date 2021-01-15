2c67ba0b9d10ff3d5a64f9401f8218955078063d
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-15 02:18:36 +0100

    Update ShortenedUrl.ts
    Removed VisitData

--- src/models/ShortenedUrl.ts
+++ src/models/ShortenedUrl.ts
@@ -1,13 +1,7 @@
-type VisitData = {
-	shortenedUrlId: string;
-	date: Date;
-};
-
 export default interface ShortenedUrl {
 	id: string;
-	createdAt: Date;
 	name: string;
-	shortUrl: string;
-	longUrl: string;
-	visits: Array<VisitData>;
+	createdAt: Date;
+	shortURL: string;
+	longURL: string;
 }
