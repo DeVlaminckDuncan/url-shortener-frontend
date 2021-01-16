f3f1bdde26ed25e823decab45e3e170cdc7ce41b
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-16 01:56:10 +0100

    Update dataFormattings.ts
    - Added addPaddingZeros()
    - Added formatDate()

--- src/utils/dataFormattings.ts
+++ src/utils/dataFormattings.ts
@@ -1,1 +1,5 @@
 export const capitalize = (s: string): string => `${s[0].toUpperCase()}${s.slice(1)}`;
+
+export const addPaddingZeros = (time: number): string => time.toString().padStart(2, '0');
+
+export const formatDate = (d: Date): string => `${addPaddingZeros(d.getHours())}:${addPaddingZeros(d.getMinutes())} ${addPaddingZeros(d.getDate())}/${addPaddingZeros(d.getMonth() + 1)}/${d.getFullYear()}`;
