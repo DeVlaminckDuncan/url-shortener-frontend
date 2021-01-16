1ff8fb685c787390f13ab3c7f2576a500efa3661
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-16 15:16:36 +0100

    Update App.vue
    Removed the header

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,6 @@
 <template>
-	<div id="nav">
-		<router-link to="/">Home</router-link>
-		<span v-if="loggedIn">
-			|
-			<router-link to="/dashboard">Dashboard</router-link>
-		</span>
-		<span v-if="!loggedIn">
-			|
-			<router-link to="/login">Login</router-link>
-			|
-			<router-link to="/signup">Sign up</router-link>
-		</span>
-		<span v-else>
-			|
-			<button @click="logOut()" class="font-semibold">Log out</button>
-		</span>
-	</div>
 	<router-view />
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-import route from '@/router';
-
-import cookie from '@/utils/cookie';
-
-export default defineComponent({
-	setup() {
-		const loggedIn = ref(cookie.get('token') ? true : false);
-
-		const logOut = () => {
-			cookie.delete('token');
-
-			route.push('/');
-
-			loggedIn.value = false;
-		};
-
-		return {
-			loggedIn,
-			logOut,
-		};
-	},
-});
-</script>
 
 <style lang="scss">
 #app {
