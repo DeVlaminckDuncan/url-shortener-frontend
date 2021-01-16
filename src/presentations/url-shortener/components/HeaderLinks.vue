70011ad5f3a71045da902791667ea1343960fb0a
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-16 15:16:17 +0100

    Create HeaderLinks.vue

---
+++ src/presentations/url-shortener/components/HeaderLinks.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="nav">
+		<router-link to="/">Home</router-link>
+		<span v-if="loggedIn">
+			|
+			<router-link to="/dashboard">Dashboard</router-link>
+		</span>
+		<span v-if="!loggedIn">
+			|
+			<router-link to="/login">Login</router-link>
+			|
+			<router-link to="/signup">Sign up</router-link>
+		</span>
+		<span v-else>
+			|
+			<button @click="logOut()" class="font-semibold">Log out</button>
+		</span>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import route from '@/router';
+
+import cookie from '@/utils/cookie';
+
+export default defineComponent({
+	setup() {
+		const loggedIn = ref(cookie.get('token') ? true : false);
+
+		const logOut = () => {
+			cookie.delete('token');
+
+			route.push('/');
+
+			loggedIn.value = false;
+		};
+
+		return {
+			loggedIn,
+			logOut,
+		};
+	},
+});
+</script>
