699ab7a3eff2cfb9c28ed3d7876ca8cc8a1129fa
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-16 16:26:38 +0100

    Update Home.vue
    Show different messages based on whether the user is logged in or not

--- src/presentations/url-shortener/views/Home.vue
+++ src/presentations/url-shortener/views/Home.vue
@@ -2,19 +2,35 @@
 	<HeaderLinks />
 
 	<main class="mt-6">
-		<p class="mb-2 text-lg">Welcome to URL Shortener!</p>
-		<p class="text-lg">Get started by <router-link to="/signup" class="text-blue-main">creating an account.</router-link></p>
+		<div v-if="loggedIn">
+			Welcome back!
+			<p>Check your links on the <router-link to="/dashboard" class="text-blue-main">dashboard</router-link></p>
+		</div>
+
+		<div v-else>
+			<p class="mb-2 text-lg">Welcome to URL Shortener!</p>
+			<p class="text-lg">Get started by <router-link to="/signup" class="text-blue-main">creating an account.</router-link></p>
+		</div>
 	</main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
+import cookie from '@/utils/cookie';
 import HeaderLinks from '../components/HeaderLinks.vue';
 
 export default defineComponent({
 	components: {
 		HeaderLinks,
+	},
+
+	setup() {
+		const loggedIn = ref(cookie.get('token') ? true : false);
+
+		return {
+			loggedIn,
+		};
 	},
 });
 </script>
