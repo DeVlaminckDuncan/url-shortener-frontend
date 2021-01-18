72bf6276060143787e0f81dbc69b79d06132647d
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-18 14:36:45 +0100

    Added VueApexCharts package

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// @ts-ignore
+import VueApexCharts from 'vue3-apexcharts';
+
 import './assets/tailwind.css';
 
 createApp(App)
+	.use(VueApexCharts)
 	.use(store)
 	.use(router)
 	.mount('#app');
