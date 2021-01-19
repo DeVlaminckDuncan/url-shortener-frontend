d4bf1b36e1706d01f5598b1cae7872714380ed0c
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-20 00:26:54 +0100

    Added some loading icons

--- src/presentations/url-shortener/views/Login.vue
+++ src/presentations/url-shortener/views/Login.vue
@@ -21,6 +21,15 @@
 				</p>
 			</div>
 
+			<div v-if="state.loading" class="flex justify-center mb-8">
+				<svg width="48px" height="48px" viewBox="0 0 128 128">
+					<g>
+						<path d="M64 0a7 7 0 1 1-7 7 7 7 0 0 1 7-7zm29.86 12.2a2.8 2.8 0 1 1-3.83 1.02 2.8 2.8 0 0 1 3.83-1.02zm22.16 21.68a3.15 3.15 0 1 1-4.3-1.15 3.15 3.15 0 0 1 4.3 1.15zm.87 60.53a4.2 4.2 0 1 1-1.57-5.7 4.2 4.2 0 0 1 1.54 5.73zm7.8-30.5a3.85 3.85 0 1 1-3.85-3.85 3.85 3.85 0 0 1 3.85 3.84zm-30 53.2a4.55 4.55 0 1 1 1.66-6.23 4.55 4.55 0 0 1-1.67 6.22zM64 125.9a4.9 4.9 0 1 1 4.9-4.9 4.9 4.9 0 0 1-4.9 4.9zm-31.06-8.22a5.25 5.25 0 1 1 7.17-1.93 5.25 5.25 0 0 1-7.14 1.93zM9.9 95.1a5.6 5.6 0 1 1 7.65 2.06A5.6 5.6 0 0 1 9.9 95.1zM1.18 63.9a5.95 5.95 0 1 1 5.95 5.94 5.95 5.95 0 0 1-5.96-5.94zm8.1-31.6a6.3 6.3 0 1 1 2.32 8.6 6.3 6.3 0 0 1-2.3-8.6zM32.25 8.87a6.65 6.65 0 1 1-2.44 9.1 6.65 6.65 0 0 1 2.46-9.1z" fill="#000000" fill-opacity="1" />
+						<animateTransform attributeName="transform" type="rotate" values="0 64 64;30 64 64;60 64 64;90 64 64;120 64 64;150 64 64;180 64 64;210 64 64;240 64 64;270 64 64;300 64 64;330 64 64" calcMode="discrete" dur="1080ms" repeatCount="indefinite"></animateTransform>
+					</g>
+				</svg>
+			</div>
+
 			<div class="flex justify-center items-center">
 				<Button @click="submit" text="Log in" color="blue-main" :classes="['px-6', 'py-2', 'rounded-lg', 'transition', 'duration-75', 'mr-4']" />
 				<router-link to="/signup" class="text-blue-main">Sign up</router-link>
@@ -64,6 +73,8 @@ export default defineComponent({
 				login: '',
 				password: '',
 			},
+
+			loading: false,
 		});
 
 		const submit = async (event: Event) => {
@@ -98,12 +109,20 @@ export default defineComponent({
 					delete loginData.email;
 				}
 
+				setTimeout(() => {
+					state.loading = true;
+				}, 1000);
+
 				const data = await post('login', loginData);
 
-				if (data.statusCode == 'NON_EXISTING_USER') {
-					state.loginError = 'This username or email does not exist.';
-				} else if (data.statusCode == 'WRONG_PASSWORD') {
-					state.passwordError = 'Wrong password.';
+				if (data.statusCode != 'OK') {
+					state.loading = false;
+
+					if (data.statusCode == 'NON_EXISTING_USER') {
+						state.loginError = 'This username or email does not exist.';
+					} else if (data.statusCode == 'WRONG_PASSWORD') {
+						state.passwordError = 'Wrong password.';
+					}
 				}
 
 				const decodedToken = decodeToken(data.token);
--- src/presentations/url-shortener/views/SignUp.vue
+++ src/presentations/url-shortener/views/SignUp.vue
@@ -6,8 +6,8 @@
 	<main class="mt-8">
 		<form>
 			<div class="flex flex-col items-center">
-				<p v-if="error" class="text-red mb-8">
-					{{ error }}
+				<p v-if="state.error" class="text-red mb-8">
+					{{ state.error }}
 				</p>
 
 				<InputField v-model="inputData.firstName" label="First name" name="firstName" id="firstName" />
@@ -15,6 +15,15 @@
 				<InputField v-model="inputData.username" label="Username" name="username" id="username" />
 				<InputField v-model="inputData.email" label="Email" name="email" id="email" type="email" />
 				<InputField v-model="inputData.password" label="Password" name="password" id="password" type="password" />
+			</div>
+
+			<div v-if="state.loading" class="flex justify-center mb-8">
+				<svg width="48px" height="48px" viewBox="0 0 128 128">
+					<g>
+						<path d="M64 0a7 7 0 1 1-7 7 7 7 0 0 1 7-7zm29.86 12.2a2.8 2.8 0 1 1-3.83 1.02 2.8 2.8 0 0 1 3.83-1.02zm22.16 21.68a3.15 3.15 0 1 1-4.3-1.15 3.15 3.15 0 0 1 4.3 1.15zm.87 60.53a4.2 4.2 0 1 1-1.57-5.7 4.2 4.2 0 0 1 1.54 5.73zm7.8-30.5a3.85 3.85 0 1 1-3.85-3.85 3.85 3.85 0 0 1 3.85 3.84zm-30 53.2a4.55 4.55 0 1 1 1.66-6.23 4.55 4.55 0 0 1-1.67 6.22zM64 125.9a4.9 4.9 0 1 1 4.9-4.9 4.9 4.9 0 0 1-4.9 4.9zm-31.06-8.22a5.25 5.25 0 1 1 7.17-1.93 5.25 5.25 0 0 1-7.14 1.93zM9.9 95.1a5.6 5.6 0 1 1 7.65 2.06A5.6 5.6 0 0 1 9.9 95.1zM1.18 63.9a5.95 5.95 0 1 1 5.95 5.94 5.95 5.95 0 0 1-5.96-5.94zm8.1-31.6a6.3 6.3 0 1 1 2.32 8.6 6.3 6.3 0 0 1-2.3-8.6zM32.25 8.87a6.65 6.65 0 1 1-2.44 9.1 6.65 6.65 0 0 1 2.46-9.1z" fill="#000000" fill-opacity="1" />
+						<animateTransform attributeName="transform" type="rotate" values="0 64 64;30 64 64;60 64 64;90 64 64;120 64 64;150 64 64;180 64 64;210 64 64;240 64 64;270 64 64;300 64 64;330 64 64" calcMode="discrete" dur="1080ms" repeatCount="indefinite"></animateTransform>
+					</g>
+				</svg>
 			</div>
 
 			<div class="flex justify-center items-center">
@@ -26,7 +35,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 import route from '@/router';
 import store, { MutationTypes } from '@/store';
 
@@ -53,14 +62,20 @@ export default defineComponent({
 			password: '',
 		};
 
-		const error = ref('');
+		const state = reactive({
+			error: '',
+			loading: false,
+		});
 
 		const submit = async (event: Event) => {
 			event.preventDefault();
 
 			if (inputData.firstName != '' && inputData.lastName != '' && inputData.username != '' && inputData.email != '' && inputData.password != '') {
-				error.value = '';
+				state.error = '';
 
+				setTimeout(() => {
+					state.loading = true;
+				}, 1000);
 				const data = await post('signup', inputData);
 
 				const decodedToken = decodeToken(data.token);
@@ -71,13 +86,13 @@ export default defineComponent({
 				route.push('/home');
 				window.location.reload();
 			} else {
-				error.value = 'Fill in all the fields!';
+				state.error = 'Fill in all the fields!';
 			}
 		};
 
 		return {
 			inputData,
-			error,
+			state,
 
 			submit,
 		};
